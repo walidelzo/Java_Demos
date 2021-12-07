5347103f50ce5e1baaeb1e7bcf7ba497d8a64c33
walidelzo <walidezo@gmail.com>
2021-12-07 14:26:37 +0200

    new changes

---
+++ src/com/company/threads/TestNewThread.java
@@ -0,0 +1,14 @@
+
+/*-----------------------------------------------------------------------------
+ - Copyright (c) Walid Elzohery ٧‏/١٢‏/٢٠٢١ ٢:٢٠ م.                           -
+ - All right Reserved.                                                        -
+ -----------------------------------------------------------------------------*/
+
+package com.company.threads;
+
+public class TestNewThread {
+    public static void main(String[] args) {
+            ThreadTestNew threadTestNew = new ThreadTestNew();
+            threadTestNew.start();
+    }
+}
