e000095aff2aef342dd1b84a496d6f5800c24c90
walidelzohery <walidelzo@gmail.com>
2021-12-07 09:14:33 +0200

    jdbc test class

--- src/com/company/threads/snychronnization/TeacherStudentWhiteBoardChallenge.java
+++ src/com/company/threads/snychronnization/TeacherStudentWhiteBoardChallenge.java
@@ -4,6 +4,42 @@
  -----------------------------------------------------------------------------*/
 
 package com.company.threads.snychronnization;
+class WhiteBoard{
+    String  text;
+    public void write(String text){
+        this.text = text;
+    }
+    public String read(){
+        return text;
+    }
 
+}
+class Teacher extends Thread{
+    WhiteBoard whiteBoard;
+    Teacher(WhiteBoard whiteBoard){
+        this.whiteBoard=whiteBoard;
+    }
+    @Override
+    public void run(){
+        whiteBoard.write("first statement");
+    }
+}
+class Student extends Thread{
+    WhiteBoard whiteBoard;
+    Student(WhiteBoard whiteBoard){
+        this.whiteBoard=whiteBoard;
+    }
+    @Override
+    public void run(){
+       String text= whiteBoard.read();
+        System.out.println("Student read" + text);
+    }
+}
 public class TeacherStudentWhiteBoardChallenge {
+    WhiteBoard whiteBoard = new WhiteBoard();
+    Teacher teacher = new Teacher(whiteBoard);
+    Student student1 = new Student(whiteBoard);
+    Student student2 = new Student(whiteBoard);
+    Student student3 = new Student(whiteBoard);
+
 }
